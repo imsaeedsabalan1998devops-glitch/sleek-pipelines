a753564bbe7aa99ce5afe4562c9c4a464e5daef6
imsaeedsabalan1998devops-glitch <imsaeedsabalan1998devops@gmail.com>
2026-06-12 08:21:49 +0330

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,21 @@
 import logging
+import yaml
+
+# Load config
+with open("config.yaml", "r") as f:
+    config = yaml.safe_load(f)
 
 # Configure logging
 logging.basicConfig(
-    level=logging.INFO,
+    level=config["app"]["log_level"],
     format="%(asctime)s [%(levelname)s] %(message)s"
 )
 
 logger = logging.getLogger(__name__)
 
 def run():
-    logger.info("Application started")
-    return "DevOps Playground is alive 🔥"
+    logger.info("Application started with config")
+    return config["app"]["message"]
 
 if __name__ == "__main__":
     output = run()
